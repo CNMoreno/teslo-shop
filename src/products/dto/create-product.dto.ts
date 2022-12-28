22727327bb2aa993552edae4ba73cee2af6a14db
Cristian Moreno <mateomoreno123444@gmail.com>
2022-12-28 14:22:02 -0500

    Add Documentation api

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,82 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    default: 'Product title',
+    description: 'Product Title',
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    default: 0,
+    description: 'Product price',
+    required: false,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    default: 'Reprehenderit enim ullamco anim veniam nisi.',
+    description: 'Product description',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    default: 'product_title',
+    description: 'Product slug',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    default: 10,
+    description: 'Product stock',
+    required: false,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    default: ['M', 'L', 'XL', 'XXL'],
+    description: 'Product sizes',
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    default: 'women',
+    description: 'Product gender',
+    enum: ['men', 'women', 'kid', 'unisex'],
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    default: ['red', 'shirt', 'pants'],
+    description: 'Product tags',
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    default: ['https://image1.com'],
+    description: 'Product images',
+    required: false,
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
