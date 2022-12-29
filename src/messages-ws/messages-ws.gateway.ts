f6fa5f48d563f80a8efaae31f2d5af4bf8ea1fcf
Cristian Moreno <mateomoreno123444@gmail.com>
2022-12-28 22:59:00 -0500

    Add Sockets

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,21 +1,41 @@
+import { SubscribeMessage, WebSocketServer } from '@nestjs/websockets';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
-  
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+
+    this.wss.emit('message-from-server', {
+      fullName: 'yo',
+      message: payload.message,
+    });
   }
 }
